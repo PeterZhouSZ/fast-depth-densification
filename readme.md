29d589f629d7b82b4e142cbf6ac661598032eef0
dimitri <ddiakopoulos@gmail.com>
2019-04-22 21:37:21 -0700

    added readme

---
+++ readme.md
@@ -0,0 +1,18 @@
+# fast-depth-densification
+
+An implementation of [Fast Depth Densification for Occlusion-Aware Augmented Reality](https://homes.cs.washington.edu/~holynski/publications/occlusion/index.html) in C++ with minimal dependencies.
+
+## Publication
+
+This work is based on the following research paper:
+
+```
+@article{Occlusion2018,
+        author    = {Aleksander Holynski and Johannes Kopf},
+        title     = {Fast Depth Densification for Occlusion-aware Augmented Reality},
+        booktitle = {ACM Transactions on Graphics (Proc. SIGGRAPH Asia)},
+        publisher = {ACM},
+        volume = {37},
+        number = {6},
+        year = {2018}}
+```
--- src/app.cpp
+++ src/app.cpp
@@ -1,30 +1,3 @@
-/* 
- * This is free and unencumbered software released into the public domain.
- * 
- * Anyone is free to copy, modify, publish, use, compile, sell, or
- * distribute this software, either in source code form or as a compiled
- * binary, for any purpose, commercial or non-commercial, and by any
- * means.
- * 
- * In jurisdictions that recognize copyright laws, the author or authors
- * of this software dedicate any and all copyright interest in the
- * software to the public domain. We make this dedication for the benefit
- * of the public at large and to the detriment of our heirs and
- * successors. We intend this dedication to be an overt act of
- * relinquishment in perpetuity of all present and future rights to this
- * software under copyright law.
- * 
- * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
- * EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
- * MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
- * IN NO EVENT SHALL THE AUTHORS BE LIABLE FOR ANY CLAIM, DAMAGES OR
- * OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE,
- * ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
- * OTHER DEALINGS IN THE SOFTWARE.
- * 
- * For more information, please refer to <http://unlicense.org/>
- */
-
 #include "util.hpp"
 #include "gl-imgui.hpp"
 #include "gl-geometry.hpp"
@@ -37,7 +10,7 @@ std::unique_ptr<gui::imgui_manager> imgui;
 
 int main(int argc, char * argv[])
 {
-    try { win.reset(new window(1280, 720, "gfx-app")); }
+    try { win.reset(new window(1280, 720, "fast-depth-densification")); }
     catch (const std::exception & e) { std::cout << "Caught GLFW window exception: " << e.what() << std::endl; }
     
     imgui.reset(new gui::imgui_manager(win->get_glfw_window_handle()));
